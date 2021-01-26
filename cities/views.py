00ea4feebf70ca9462af8dd51936ecf5c5c9439a
Alexandr Mochalov <Alexander.Mochalov@onlyoffice.com>
2021-01-26 19:49:37 +0300

    Отображение городов на главной странице

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from cities.models import City
+
+__all__ = (
+    'home',
+)
+
+# функция отображения городов
+# получение списка всех городов и передача в контекстное меню
+# context - словарь со списком городов
+
+
+def home(request):
+    qs = City.objects.all()
+    context = {'objects_list': qs}
+    return render(request, 'cities/home.html', context)
